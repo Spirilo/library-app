c8a95ea76ed8ba1b4e4f03aecbd4491c972d316e
Niko <rappe.niko@gmail.com>
2023-06-01 11:18:46 +0300

    basic CRUD for book

--- library/src/main/java/com/example/niko/library/controllers/BookController.java
+++ library/src/main/java/com/example/niko/library/controllers/BookController.java
@@ -3,11 +3,19 @@ package com.example.niko.library.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.niko.library.entities.Book;
+import com.example.niko.library.entities.Info;
 import com.example.niko.library.repositories.BookRepository;
 
 @RestController
@@ -19,6 +27,33 @@ public class BookController {
 	@GetMapping
 	List<Book> getAll() {
 		return repo.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	Book get(@PathVariable int id) {
+		Book b = repo.findById(id).orElse(null);
+		if (b == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No book by id in database");
+		return b;
+	}
+	
+	@PostMapping
+	Book create(@RequestBody Book b) {
+		repo.saveAndFlush(b);
+		return b;
+	}
+	
+	@PutMapping("/{id}")
+	Book save(@PathVariable int id, @RequestBody Book b) {
+		repo.saveAndFlush(b);
+		return b;
+	}
+	
+	@DeleteMapping("/{id}") 
+	Info delete(@PathVariable int id) {
+		Book b = repo.findById(id).orElse(null);
+		if (b == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No book by id in database");
+		repo.deleteById(id);
+		return new Info("Deleted book!");
 	}
 
 }
---
+++ library/src/main/java/com/example/niko/library/entities/Info.java
@@ -0,0 +1,18 @@
+package com.example.niko.library.entities;
+
+public class Info {
+	private String msg;
+
+	public Info(String message) {
+		msg = message;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+}
