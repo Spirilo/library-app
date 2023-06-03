a2ce6865ca1299523f27ee72ab309d96b0c17458
Niko <rappe.niko@gmail.com>
2023-06-03 10:16:02 +0300

    login works for user

--- library/src/main/java/com/example/niko/library/security/LoginController.java
+++ library/src/main/java/com/example/niko/library/security/LoginController.java
@@ -1,12 +1,28 @@
 package com.example.niko.library.security;
 
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.*;
 
 @RestController
 @RequestMapping("/login")
@@ -25,10 +41,32 @@ public class LoginController {
 		user.setPassword(pw);
 		String role = "user";
 		if (user.getRole()!=null) role = user.getRole();
-		//user.setEnabled(true);
 		user.setRole(role);
 		repo.save(user);
 		return user;
+	}
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@PutMapping
+	public ResponseEntity<?> login(@RequestBody User u, HttpServletRequest req) throws Exception {
+		User userRet = null;
+		try {
+			Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(u.getUsername(), u.getPassword()));
+			SecurityContext sc = SecurityContextHolder.getContext();
+			sc.setAuthentication(auth);
+			HttpSession session = req.getSession(true);		
+			session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
+			UserPrincipal up = (UserPrincipal)auth.getPrincipal();
+			userRet = up.getUser();
+			userRet.setPassword(""); // Do not show the crypted password
+			System.out.println("put "+auth.isAuthenticated()+","+auth.getName()+","+auth.getPrincipal()+","+
+					auth.getAuthorities().stream().map(x -> x.getAuthority()).collect(Collectors.joining(":")));
+		} catch (BadCredentialsException e) {
+			return new ResponseEntity<LoginResponse>(new LoginResponse("Bad credentials"),HttpStatus.FORBIDDEN);
+		}
+		return new ResponseEntity<User>(userRet,HttpStatus.OK);
 	}
 
 }
