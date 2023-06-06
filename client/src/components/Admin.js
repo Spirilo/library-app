ae1a22325f9979aaddf9a049732d880ce2ba7d10
Niko <rappe.niko@gmail.com>
2023-06-06 15:32:30 +0300

    added input fields to add book and author

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,9 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import authorService from '../services/authors'
 
 export const Admin = () => {
   const [value, setValue] = useState('')
+  const [authors, setAuthors] = useState([])
 
-  console.log(value)
+  useEffect(() => {
+    const getData = async () => {
+      const data = await authorService.getAll()
+      setAuthors(data)
+    }
+    getData()
+  }, [])
+
+  const rows = authors.map(a => <option key={a.id}>{a.lastName}, {a.firstName}</option>)
   return(
     <div>
     <h2>Admin view</h2>
@@ -13,6 +23,24 @@ export const Admin = () => {
       <option value="book">Book</option>
       <option value="author">Author</option>
     </select>
+    {value === 'book' && 
+      <div>
+        <p>Title: <input type="text" /></p>
+        <p>Year: <input type="number" /></p>
+        Author: 
+        <select>
+          {rows}
+        </select>
+        <p><button>Add</button></p>
+      </div>
+    }
+    {value === 'author' && 
+      <div>
+        <p>Firstname: <input type="text" /></p>
+        <p>Lastname: <input type="number" /></p>
+        <button>Add</button>
+      </div>
+    }
     </div>
   )
 }
