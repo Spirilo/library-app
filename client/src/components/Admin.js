ccdd1bddc2659c6cae360f5e0a8748176e278821
Niko <rappe.niko@gmail.com>
2023-06-06 15:08:26 +0300

    dropdown to admin view to choose what to add

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,5 +1,18 @@
+import { useState } from "react"
+
 export const Admin = () => {
+  const [value, setValue] = useState('')
+
+  console.log(value)
   return(
+    <div>
     <h2>Admin view</h2>
+    <p>Choose what to add</p>
+    <select onChange={ev => setValue(ev.target.value)} value={value}>
+      <option value="">....</option>
+      <option value="book">Book</option>
+      <option value="author">Author</option>
+    </select>
+    </div>
   )
 }
